1c7074390af6a7f91604ac4d08025cf777c06096
Andres Lucero Soria <andres022lucero@gmail.com>
2026-01-13 12:57:35 -0400

    commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,29 @@
+import BotonSimple from "../../components/BotonSimple/Index"
+import styles from './Home.module.css'
+
+
+const Home = () => {
+  return (
+    <div className={styles.home__container}>
+        <h1>DIZI STORE</h1>
+        <h2>VESTIDOS</h2>
+        <div>
+            <BotonSimple>XS</BotonSimple>
+            <BotonSimple>S</BotonSimple>
+            <BotonSimple>M</BotonSimple>
+            <BotonSimple>L</BotonSimple>
+            <BotonSimple>XL</BotonSimple>
+            <BotonSimple>XXL</BotonSimple>
+        </div>
+        <div>
+            <p>
+            ¡Bienvenidos a DIZI STORE! 🌸✨
+            En nuestra tienda online encontrarás vestidos, tacones, maquillaje y más, pensados para que luzcas increíble en cada ocasión. Nos ubicamos en Cochabamba, Bolivia, y realizamos envíos a todos los departamentos y provincias del país, para que disfrutes de tus compras sin importar dónde estés. Calidad, estilo y comodidad al alcance de un clic.
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
