6a2495908a691db96cd7b1e65ad76a491a45dba7
Andres Lucero Soria <andres022lucero@gmail.com>
2026-01-18 18:32:39 -0400

    Se añadio las Redes sociales al menu desplegable

--- src/pages/Favoritos/Index.tsx
+++ src/pages/Favoritos/Index.tsx
@@ -3,6 +3,7 @@ import vestidos from '../../data/vestidos.json'
 import type {Vestido} from "../../types/Vestidos"
 import TarjetaHome from '../../components/TarjetaHome/Index';
 import { useState, useEffect } from 'react';
+import styles from './Favoritos.module.css'
 
 type Props = {}
 
@@ -43,18 +44,21 @@ function Favoritos({}: Props) {
     
   return (
     <>
-      <Header></Header>
-      <div>
-        {datosVestidos.map((item, index: number) => (
-         <>
-          {
-          idFavoritos.some(elem => elem === item.id ) &&
-          
-          
-          <TarjetaHome key={index} imagen={item.image[0]} nombreProducto={item.name} slug={item.slug} handleFavoritos={()=>funcionFavoritos(item.id)}></TarjetaHome>
-        }
-         </>
-        ))}
+      <div className={styles.favoritos__contenedor}>
+        <Header></Header>
+        <h1 className={styles.favoritos__h1}>TUS FAVORITOS</h1>
+        <div className={styles.favorito__elemento}>
+          {datosVestidos.map((item, index: number) => (
+          <>
+            {
+            idFavoritos.some(elem => elem === item.id ) &&
+            
+            
+            <TarjetaHome key={index} imagen={item.image[0]} nombreProducto={item.name} slug={item.slug} handleFavoritos={()=>funcionFavoritos(item.id)}></TarjetaHome>
+          }
+          </>
+          ))}
+        </div>
       </div>
     </>
     )
