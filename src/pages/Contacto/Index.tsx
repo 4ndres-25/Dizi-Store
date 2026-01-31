900d56af991f5a1e2fcaa86cf576d9e01f559c59
Alejandro Lucero Soria <luceroale017@gmail.com>
2026-01-30 20:28:33 -0400

    pagina contacto arreglado

--- src/pages/Contacto/Index.tsx
+++ src/pages/Contacto/Index.tsx
@@ -1,18 +1,23 @@
-
+import styles from "./Contacto.module.css"
 
 type Props = {}
 
 function Contacto({}: Props) {
   return (
-    <div>
-        <h1>Contacto</h1>
+    <div className={styles.contacto__container}>
+        <h1 className={styles.contacto__h1}>Contacto</h1>
+        <div className={styles.contacto__tarjeta}>
         <p>Si deseas consultar sobre algún producto, precios, tallas o disponibilidad, puedes escribirnos por WhatsApp o mediante nuestros canales de contacto.
+          <br /><br />
+          <span className={styles.contacto__span}>WhatsApp:</span> +591 72234794
+          <br />
+          <span className={styles.contacto__span}>Correo electrónico: </span>
+          <br />
+          <span className={styles.contacto__span}>Horario de atención: </span>          
 
-WhatsApp: +591 72234794
-Correo electrónico: (opcional)
+          Lunes a Domingo — 09:00 a 20:00 (Bolivia)</p>
 
-Horario de atención orientativo:
-Lunes a Domingo — 09:00 a 20:00 (Bolivia)</p>
+        </div>
     </div>
   )
 }
