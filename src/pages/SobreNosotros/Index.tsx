10d3a3abb576e6a5091d30486b23855ed3480d71
Alejandro Lucero Soria <luceroale017@gmail.com>
2026-01-30 20:44:33 -0400

    cambios en paginas legales

--- src/pages/SobreNosotros/Index.tsx
+++ src/pages/SobreNosotros/Index.tsx
@@ -1,11 +1,11 @@
-
+import styles from "./SobreNosotros.module.css"
 type Props = {}
 
 function SobreNosotros({}: Props) {
   return (
-    <div>
-        <h1>Sobre Nosotros</h1>
-        <p>Somos un catálogo online de ropa y accesorios dirigido al público de Bolivia. Nuestro objetivo es mostrar productos como vestidos, carteras, mochilas y otros artículos de moda para que los usuarios puedan conocerlos y contactar directamente para consultas o compras.
+    <div className={styles.nosotros__container}>
+        <h1 className={styles.nosotros__titulo}>Sobre Nosotros</h1>
+        <p>Somos un catálogo online de ropa y accesorios dirigido al público de Bolivia. Nuestro objetivo es mostrar productos como vestidos y otros artículos de moda para que los usuarios puedan conocerlos y contactar directamente para consultas o compras.
 
 No realizamos pagos dentro del sitio web. Si algún producto te interesa, puedes consultarnos por WhatsApp para más información sobre precios, tallas, colores o disponibilidad.
 
