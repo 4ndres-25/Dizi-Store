7f071ad768423a643451eb80efdf9f7b639a1bd2
Andres Lucero Soria <andres022lucero@gmail.com>
2026-03-03 19:43:01 -0400

    Menu desplegable con fondo y correccion del tamaño del h1 de Producto Seleccionado

--- src/components/Menu/Index.tsx
+++ src/components/Menu/Index.tsx
@@ -23,34 +23,36 @@ function Menu({}: Props) {
       </button>
       
       
-      <div className={`${styles["menu__main-container--off"]} ${showMenu ? styles["menu__main-container--on"] : ""}`} >
-        <button className={styles.menu__iconClose} onClick={handleOpenMenu}><IoCloseSharp /></button>
-        <div >
-          <h2 className={styles.menu__h2}>SIGUENOS EN NUESTRAS REDES SOCIALES</h2>
-          <div className={styles.menu__redesSociales}>
-            <a className={`${styles.menu__redSocial} ${styles["menu__redSocial--facebook"]}`} href="https://www.facebook.com/share/17j4H6Ln84/" target="_blank">
-              <div className={`${styles.menu__redCirculo} ${styles["menu__redCirculo--facebook"]}`}>
-                <FaFacebook className={`${styles.menu__redIconFacebook}`}/>
-              </div>
-              <p>Dizi Store</p>
-            </a>
+      <div className={`${showMenu ? styles["menu__containerFondo--on"] : "menu__containerFondo--off"}`}>
+      </div>    
+        <div className={`${styles["menu__main-container--off"]} ${showMenu ? styles["menu__main-container--on"] : ""}`} >
+          <button className={styles.menu__iconClose} onClick={handleOpenMenu}><IoCloseSharp /></button>
+          <div >
+            <h2 className={styles.menu__h2}>SÍGUENOS EN NUESTRAS REDES SOCIALES</h2>
+            <div className={styles.menu__redesSociales}>
+              <a className={`${styles.menu__redSocial} ${styles["menu__redSocial--facebook"]}`} href="https://www.facebook.com/share/17j4H6Ln84/" target="_blank">
+                <div className={`${styles.menu__redCirculo} ${styles["menu__redCirculo--facebook"]}`}>
+                  <FaFacebook className={`${styles.menu__redIconFacebook}`}/>
+                </div>
+                <p>Dizi Store</p>
+              </a>
 
-            <a className={`${styles.menu__redSocial} ${styles["menu__redSocial--whatsapp"]}`} href="https://wa.me/59172234794" target="_blank">
-              <div className={`${styles.menu__redCirculo} ${styles["menu__redCirculo--whatsapp"]}`}>
-                <IoLogoWhatsapp className={`${styles.menu__redIconWhatsapp}`}/>
-              </div>
-              <p>Dizi Store</p>
-            </a>
+              <a className={`${styles.menu__redSocial} ${styles["menu__redSocial--whatsapp"]}`} href="https://wa.me/59172234794" target="_blank">
+                <div className={`${styles.menu__redCirculo} ${styles["menu__redCirculo--whatsapp"]}`}>
+                  <IoLogoWhatsapp className={`${styles.menu__redIconWhatsapp}`}/>
+                </div>
+                <p>Dizi Store</p>
+              </a>
 
-            <a className={`${styles.menu__redSocial} ${styles["menu__redSocial--tiktok"]}`} href="https://www.tiktok.com/@dizistore.bol" target="_blank">
-              <div className={`${styles.menu__redCirculo} ${styles["menu__redCirculo--tiktok"]}`}>
-                <FaTiktok className={`${styles.menu__redIconTiktok}`}/>
-              </div>
-              <p>Dizi Store</p>
-            </a>
+              <a className={`${styles.menu__redSocial} ${styles["menu__redSocial--tiktok"]}`} href="https://www.tiktok.com/@dizistore.bol" target="_blank">
+                <div className={`${styles.menu__redCirculo} ${styles["menu__redCirculo--tiktok"]}`}>
+                  <FaTiktok className={`${styles.menu__redIconTiktok}`}/>
+                </div>
+                <p>Dizi Store</p>
+              </a>
+            </div>
           </div>
-        </div>
-      </div>      
+        </div>    
     
 
       
